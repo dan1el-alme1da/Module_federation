ca21dc2108407cf51b9876cdf9f2ed1c0504cfab
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-02-06 16:14:19 -0300

    feat: app,js  app.css both in app

---
+++ packages/app/src/app.js
@@ -0,0 +1,33 @@
+import React, { Suspense } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link 
+} from 'react-router-dom';
+import {
+    Navbar,
+    Nav,
+    NavItem
+} from 'reactstrap';
+
+//css
+
+const App () => {
+    return(
+        <Router>
+            <div>
+                <Navbar color="light" light expend = "md">
+                    <Nav className = "mr-auto" Navbar>
+                        <NavItem>
+                            <Link to="/">Home</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/contact">Contact</Link>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+            </div>
+        </Router>
+    )
+}
