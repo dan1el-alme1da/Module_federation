2d5f2d9dd2f87a16c2e52e0568121634f13f39ed
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-02-07 18:40:04 -0300

    feat: boostrap.js and index.js

---
+++ packages/contact/src/bootstrap.js
@@ -0,0 +1,6 @@
+import Contact from "./contact";
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Contact />, document.querySelector('#root')); 
