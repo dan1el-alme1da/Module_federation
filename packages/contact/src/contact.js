366a1c0b13823d9324f492a2f18fdaf781c80c04
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-02-07 18:32:06 -0300

    feat: created contact.js

---
+++ packages/contact/src/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './contact.css';
+
+const Contact = () => {
+    return (
+        <div id='contact-page'>
+            <h1>Pagina de Contatos</h1>
+            <p>Esta é um a simples pagina de contato</p>
+        </div>
+    )
+}
+
+export default Contact;
