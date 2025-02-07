83e677e4ec52e5e58ede7b775f97daf09ebd31e5
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-02-07 16:56:31 -0300

    feat: Created app.js

---
+++ packages/home/src/bootstrap.js
@@ -0,0 +1,6 @@
+import Home from "./home";
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Home />, document.querySelector('#root')); 
